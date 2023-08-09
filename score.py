6a73526bd293682116f34365fccc55842e14c149
osbm <osmanfbayram@gmail.com>
2023-08-09 18:58:18 +0300

    revert paths

--- score.py
+++ score.py
@@ -21,8 +21,11 @@ def DICE_COE(mask1, mask2, class_id):
 
 scores = []
 for i in range(19):
-    prediction = nib.load(f"all_to_all-outputs_post_processed/PROSTATE_{i+1}.nii.gz").get_fdata()
-    ground_truth = nib.load(f"labelsTs/PROSTATE_{i+1}.nii.gz").get_fdata()
+    # prediction = nib.load(f"all_to_all-outputs_post_processed/PROSTATE_{i+1}.nii.gz").get_fdata()
+    # ground_truth = nib.load(f"labelsTs/PROSTATE_{i+1}.nii.gz").get_fdata()
+
+    prediction = nib.load(f"test/{i+1:03}/predicted.nii.gz").get_fdata()
+    ground_truth = nib.load(f"test/{i+1:03}/t2_anatomy_reader1.nii.gz").get_fdata()
 
     for class_id in [0, 1, 2]:  # Loop over each class
         dice = DICE_COE(prediction, ground_truth, class_id)
