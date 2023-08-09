e519e3c9581c97d841f8d98f21c5c66e19000813
osbm <osmanfbayram@gmail.com>
2023-08-09 18:38:59 +0300

    update scores script

--- score.py
+++ score.py
@@ -21,14 +21,18 @@ def DICE_COE(mask1, mask2, class_id):
 
 scores = []
 for i in range(19):
-    prediction = nib.load(f"test/{i+1:03}/predicted.nii.gz").get_fdata()
-    ground_truth = nib.load(f"test/{i+1:03}/t2_anatomy_reader1.nii.gz").get_fdata()
+    prediction = nib.load(f"all_to_all-outputs_post_processed/PROSTATE_{i+1}.nii.gz").get_fdata()
+    ground_truth = nib.load(f"labelsTs/PROSTATE_{i+1}.nii.gz").get_fdata()
 
     for class_id in [0, 1, 2]:  # Loop over each class
         dice = DICE_COE(prediction, ground_truth, class_id)
         scores.append(dice)
 
-scores = np.array(scores)
-print("DICE scores:\n", scores.reshape(19, 3))
+scores = np.array(scores).reshape(19, 3)
+df = pd.DataFrame(scores)
+df.columns = ["background-dice", "inner-prostate-dice", "outer-prostate-dice"]
+df.insert(0, "image-id", np.arange(1, 20))
+df.to_csv("results.csv", index=False)
 
-print("Mean DICE scores:", np.mean(scores.reshape(19, 3), axis=0))
+print("DICE scores:\n", scores)
+print("Mean DICE scores:", np.mean(scores, axis=0))
